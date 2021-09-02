48bfc1adfc34fb031a262df9c8fc826f5f397427
Stevie Ambroise <s.ambroise@protonmail.com>
2021-09-01 21:38:20 -0600

    Add Animal Variable with Object Value

--- object-examples.js
+++ object-examples.js
@@ -1,13 +1,19 @@
 var car = {
   color: "green",
+  isFast: true,
   yearsOld: 19,
   numWheels: 3,
   noiseMade: "vroom vroom bang",
-  isFast : false,
-  yearOfAccidents = [1934, 1945, 1952, 1953]
+  yearOfAccidents: [1934, 1945, 1952, 1953]
 }
 
+var animal = {
+  isFuzzy: true,
+  noiseMade: "woof woof",
+  color: "turquoise",
+  age: 19784,
+  peopleBitten: ['Max', 'Adam', 'Lucy', 'Abem', 'Kamila']
+}
 
-var animal
 
 var customer
