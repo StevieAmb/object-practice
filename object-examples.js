1ad76cee544ce317d455b9922529e4384322ab46
Stevie Ambroise <s.ambroise@protonmail.com>
2021-09-01 20:24:01 -0600

    Add Object Variable Car

---
+++ object-examples.js
@@ -0,0 +1,13 @@
+var car = {
+  color: "green",
+  yearsOld: 19,
+  numWheels: 3,
+  noiseMade: "vroom vroom bang",
+  isFast : false,
+  yearOfAccidents = [1934, 1945, 1952, 1953]
+}
+
+
+var animal
+
+var customer
